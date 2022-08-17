c7e6b7c67f23d3fef26b29a9608ada51073bd18d
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-17 23:46:33 +1000

    Setup sequelize models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: product,
+        key: id,
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: tag,
+        key: id,
+      }
+    },
   },
   {
     sequelize,
